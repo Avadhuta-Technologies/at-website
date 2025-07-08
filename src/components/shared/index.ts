6834cc89d3c7fc6cf36dd479ae548136324d9979
Vinay <vinay@avadhutatech.com>
2025-07-08 16:51:46 +0530

    Add "Our Work" link to Header and Footer components; include WhyChooseSection in shared exports

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,4 +2,5 @@
 export { default as HeroSection } from './HeroSection.astro';
 export { default as CTASection } from './CTASection.astro';
 export { default as FAQSection } from './FAQSection.astro';
-export { default as TestimonialsSection } from './TestimonialsSection.astro'; 
+export { default as TestimonialsSection } from './TestimonialsSection.astro';
+export { default as WhyChooseSection } from './WhyChooseSection.astro'; 
