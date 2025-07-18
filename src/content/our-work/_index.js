bfe49116ce2c78d12871e325a4c9245575224acd
Vinay <vinay@avadhutatech.com>
2025-07-18 19:49:55 +0530

    Refactor Our Work page components: replace Portfolio CTA section with Case Studies section and update imports accordingly for improved content organization.

--- src/content/our-work/_index.js
+++ src/content/our-work/_index.js
@@ -3,5 +3,4 @@ export { ourWorkHeroData } from './_hero.js';
 export { ourWorkGridData } from './_grid.js';
 export { ourWorkIndustriesData } from './_industries.js';
 export { ourWorkWhyChooseData } from './_whyChoose.js';
-export { ourWorkPortfolioCTAData } from './_portfolioCTA.js';
 export { ourWorkCTAData } from './_cta.js'; 
