4a56ba7ad602a3fc1a40c7ff289cf0165cd78628
vikram_bn <vikram@reachmobile.com>
2025-07-20 13:52:50 +0530

    fix: vercel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind(), compress()]
+  integrations: [tailwind(), compress()],
+  adapter: vercelAdapter(),
 });
